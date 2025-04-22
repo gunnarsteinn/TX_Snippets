6aa327656a38dc5607fdd76fffb379ce0f949b20
Gunnar Steinn Magnússon <gunnarsteinn@gunnarsteinn.com>
2025-04-22 14:46:33 +0000

    Add script to fetch and parse JSON data from remote source

---
+++ DQ/Demo/Fetch JSON data.ps1
@@ -0,0 +1,5 @@
+$url = 'https://raw.githubusercontent.com/gunnarsteinn/staticwebapp/refs/heads/main/books.json'
+$results = Invoke-WebRequest -Uri $url -Headers $Headers -UseBasicParsing
+$items = $results.Content | ConvertFrom-Json
+$items | Select-Object -Property ID,Name,Description,Price #| Where-Object {$_.Price -gt 20 }
+
