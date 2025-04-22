c187f9e2b9f243817ec9ee464761bbbc683f471d
Gunnar Steinn Magnússon <gunnarsteinn@gunnarsteinn.com>
2025-04-22 14:53:44 +0000

    Fix Invoke-WebRequest call by removing unnecessary headers

--- DQ/Demo/Fetch JSON data.ps1
+++ DQ/Demo/Fetch JSON data.ps1
@@ -1,5 +1,6 @@
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 $url = 'https://raw.githubusercontent.com/gunnarsteinn/staticwebapp/refs/heads/main/books.json'
-$results = Invoke-WebRequest -Uri $url -Headers $Headers -UseBasicParsing
+$results = Invoke-WebRequest -Uri $url -UseBasicParsing
 $items = $results.Content | ConvertFrom-Json
 $items | Select-Object -Property ID,Name,Description,Price #| Where-Object {$_.Price -gt 20 }
 
